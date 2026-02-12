a0ed06a690003215c2ff751a2a061e0fe971ac50
Brad Kahl <bradkahl@Brads-MacBook-Pro-2.local>
2026-02-12 00:25:44 -0500

    fix mobile logo issue

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import './App.css'
 
 function App() {
   return (
-    <>
+    <main className='page'>
       <h1>Coming Soon!</h1>
       <div>
         <img className="logo" src={logo} alt="Logo" width={600} />
       </div>
-    </>
+    </main>
   )
 }
 
