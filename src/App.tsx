7acdc31ef132c4c2d8e78e4dfaa57267ab9e5064
Brad Kahl <bradkahl@Brads-MacBook-Pro-2.local>
2026-02-11 23:24:50 -0500

    add noahs photo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import NoahLogo from "./assets/noah-photo.png";
 import './App.css'
 
 function App() {
@@ -16,18 +17,11 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <h1>eNVy Creations LLC</h1>
+      <div>Coming Soon!</div>
+      <div>
+        <img src={NoahLogo} alt="Logo" width={200} />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
