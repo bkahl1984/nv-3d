c094daa8b264e9c0b7b14bbd29b0bdb7b1009d16
Brad Kahl <bradkahl@Brads-MacBook-Pro-2.local>
2026-02-11 23:41:35 -0500

    fix logo error

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import NoahLogo from "./assets/noah-photo.png";
+import logo from "/noah-photo.jpg";
 import './App.css'
 
 function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>eNVy Creations LLC</h1>
       <div>Coming Soon!</div>
       <div>
-        <img src={NoahLogo} alt="Logo" width={200} />
+        <img src={logo} alt="Logo" width={200} />
       </div>
     </>
   )
